2f1422fcdd0ea462d7494cc38d317b62e1e1fd36
Sadia-b4 <sadiabegum.cse25@gmail.com>
2026-08-14 14:22:00 +0600

    linked list

---
+++ Linked List/doubly_linked_list.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int val;
+    Node* prev;
+    Node* next;
+    
+    Node(int val){
+        this->val = val;
+        this->next = NULL ;
+        this->prev = NULL;
+    }
+};
+
+void print_forward(Node* head){
+    Node* temp = head;
+    while(temp!=NULL){
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+void print_backward(Node* tail){
+    Node* temp = tail;
+    while(temp!=NULL){
+        cout<<temp->val<<" ";
+        temp = temp->prev;
+    }
+    cout << endl;
+}
+
+void insert_at_tail(Node* &head , Node* &tail, int val){
+    Node* newNode = new Node(val);
+    if(head == NULL){
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
+}
+
+int main(){
+    Node* head = NULL;
+    Node* tail = NULL;
+    int val;
+    while(1){
+        cin>>val;
+        if(val==-1){
+            break;
+        }
+        insert_at_tail(head,tail,val);
+
+    }
+    print_forward(head);
+    print_backward(tail);
+    return 0;
+}
